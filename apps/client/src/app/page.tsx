e788d49b619ee66a3ab45fe2b1c290bf882ab4ec
jooseariias <arijoose@gmail.com>
2023-07-04 17:57:20 -0300

    build: s

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable prettier/prettier */
-// eslint-disable-next-line prettier/prettier
-import React from 'react';
-
 import Landing from './Pages/Landing';
 export default function Home() {
   return (
