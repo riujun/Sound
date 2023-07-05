ec430fe6aa0e319c082bb7501bea8649794006bd
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-07-05 14:01:44 -0300

    feat: crud basico y schema
    create, findAll, FindOne y delete funcionando

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,14 +4,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthMiddleware } from './middlewares/auth.middleware';
 import { AuthJwtModule } from './auth-jwt/auth-jwt.module';
 import { UserModule } from './user/user.module';
+import { PlaylistModule } from './playlist/playlist.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // Importa el ConfigModule sin argumentos
-    MongooseModule.forRoot(process.env.DB_HOST), // Ejemplo de uso de una variable de entorno
+    MongooseModule.forRoot(
+      `mongodb+srv://sebastiantorreiro:R1wbQ2SmKg4L3Vqj@soundawave.1sm8mt0.mongodb.net/?retryWrites=true&w=majority`,
+    ), // Ejemplo de uso de una variable de entorno
 
     AuthJwtModule,
     UserModule,
+    PlaylistModule,
   ],
 })
 export class AppModule implements NestModule {
