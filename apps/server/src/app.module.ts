1029817f6deeae3d6e1f9abfd72b7089860b1c75
Fabian Torres <fabi.ignacio1998@gmail.com>
2023-07-05 17:08:38 -0400

    feat: se Crea crud de user
    Se crea crud de user(GetAll, Delete, GetById, UpdateById)

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -16,6 +16,6 @@ import { UserModule } from './user/user.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('user');
+    consumer.apply(AuthMiddleware).forRoutes('aut');
   }
 }
--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -1,4 +1,85 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Res,
+  HttpStatus,
+  Get,
+  Delete,
+  Put,
+  Param,
+} from '@nestjs/common';
+import { UpdateUserDto } from 'src/dto/update-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAll(@Res() res) {
+    try {
+      const users = await this.userService.getAll();
+      return res.status(HttpStatus.OK).json(users);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Error interno del servidor' });
+    }
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string, @Res() res) {
+    try {
+      const user = await this.userService.getById(id);
+      if (!user) {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ message: 'Usuario no encontrado' });
+      }
+      return res.status(HttpStatus.OK).json(user);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Error interno del servidor' });
+    }
+  }
+
+  @Delete(':id')
+  async deleteById(@Param('id') id: string, @Res() res) {
+    try {
+      const deletedUser = await this.userService.deleteById(id);
+      if (!deletedUser) {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ message: 'Usuario no encontrado' });
+      }
+      return res.status(HttpStatus.OK).json(deletedUser);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Error interno del servidor' });
+    }
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Res() res,
+  ) {
+    try {
+      const updatedUser = await this.userService.updateById(id, updateUserDto);
+      if (!updatedUser) {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ message: 'Usuario no encontrado' });
+      }
+      return res.status(HttpStatus.OK).json(updatedUser);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Error interno del servidor' });
+    }
+  }
+}
