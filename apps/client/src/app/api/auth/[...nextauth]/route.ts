8a6e007e1c130b0f7059935083f2076335ff9ce5
Dreyz <54697159+JuanQuiro@users.noreply.github.com>
2023-07-07 04:06:50 +0000

    fix(auth): :bug: soluccionado errores menores de next auth

--- apps/client/src/app/api/auth/[...nextauth]/route.ts
+++ apps/client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,8 @@
-/* eslint-disable turbo/no-undeclared-env-vars */
-/* eslint-disable @typescript-eslint/require-await */
-import NextAuth, { type NextAuthOptions, type Session, type User } from 'next-auth';
-import FacebookProvider from 'next-auth/providers/facebook';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from 'next-auth/next';
 
-export const authOptions: NextAuthOptions = {
-  debug: true,
-  callbacks: {
-    session: async ({ session, user }: { session: Session; user: User }) => ({
-      ...session,
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        image: user.image,
-      },
-    }),
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      allowDangerousEmailAccountLinking: true,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-      allowDangerousEmailAccountLinking: true,
-    }),
-  ],
-};
+import { authOptions } from '@/app/components/utils/authOptions';
 
-const handler = NextAuth(authOptions) as NextAuthOptions;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
