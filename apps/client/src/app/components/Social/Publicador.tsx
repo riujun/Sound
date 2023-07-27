7926d60d19fcbbed2f50ace8ed11d5c8386996d7
Fabian Torres <fabi.ignacio1998@gmail.com>
2023-07-27 05:12:54 -0400

    build: se crear ruta para hacer publicaciones

--- apps/client/src/app/components/Social/Publicador.tsx
+++ apps/client/src/app/components/Social/Publicador.tsx
@@ -1,14 +1,121 @@
+/* eslint-disable prettier/prettier */
+'use client';
+import React, { useEffect, useState } from 'react';
+
+import { useDataUser } from '@/app/components/Auth/hooks/dataUser';
+
+interface Song {
+  _id: string;
+  name: string;
+  duration: number;
+  user: string;
+  coArtist: string;
+  price: number;
+  genre: string;
+  image: string;
+  date: string;
+  album: string;
+  src: string;
+  __v: number;
+}
+interface Cancion {
+  _id: string;
+  name: string;
+}
+
+interface Publicacion {
+  user: string;
+  contenido: string;
+  cancionSeleccionada: string;
+}
+
 export default function Buscador() {
+  const [datosPublicacion, setDatosPublicacion] = useState<Publicacion>({
+    user: '',
+    contenido: '',
+    cancionSeleccionada: '',
+  });
+  const userData = useDataUser();
+  const userID = userData?._id ?? '';
+
+  const [canciones, setCanciones] = useState<Cancion[]>([]);
+  console.log(canciones);
+  console.log(datosPublicacion);
+
+  // Simulación: Obtener las canciones disponibles del servidor
+
+  useEffect(() => {
+    const url = `http://localhost:4000/user/mysongsuploaded/64c01b43ffa500e4b825dbb5`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseData: { songs: Song[][] }) => {
+        const flattenedSongs = responseData.songs.flat();
+        setCanciones(flattenedSongs);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:4000/publications', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: userID,
+          content: datosPublicacion.contenido,
+          song: datosPublicacion.cancionSeleccionada,
+        }),
+      });
+
+      if (response.ok) {
+        console.log('Publicación creada exitosamente');
+      } else {
+        console.log('Error al crear la publicación');
+      }
+    } catch (error) {
+      console.error('Error de red', error);
+    }
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setDatosPublicacion((datosPrevios: Publicacion) => ({ ...datosPrevios, [name]: value }));
+  };
+
   return (
     <div className="m-4 w-[92%] items-center md:m-8 md:w-[95%]">
-      <div className="flex h-12 overflow-auto rounded-full border border-gray-500 bg-white focus-within:shadow-lg">
-        <div className="grid h-full w-12 place-items-center text-gray-600"></div>
-        <input
-          className="peer w-[100%] border-none pr-2 text-sm text-gray-600  outline-none"
-          type="textarea"
-          id="search"
-          placeholder="¿Qué quieres compartir hoy?"
-        />
+      <div className="flex h-12   border-gray-500 bg-white focus-within:shadow-lg">
+        <form onSubmit={handleSubmit} className="w-full">
+          <textarea
+            className="peer w-[100%] rounded-full border pr-2 text-sm text-gray-600 outline-none"
+            id="search"
+            placeholder="¿Qué quieres compartir hoy?"
+            name="contenido"
+            value={datosPublicacion.contenido}
+            onChange={handleChange}
+          />
+          <select
+            name="cancionSeleccionada"
+            value={datosPublicacion.cancionSeleccionada}
+            onChange={handleChange}
+          >
+            <option value="">-- Seleccione una canción --</option>
+            {/* Agrega una verificación antes de utilizar el método map */}
+            {canciones.map((cancion) => (
+              <option key={cancion._id} value={cancion._id}>
+                {cancion.name}
+              </option>
+            ))}
+          </select>
+          <button className="ml-5 rounded-lg border-2 p-2" type="submit">
+            Publicar
+          </button>
+        </form>
       </div>
     </div>
   );
